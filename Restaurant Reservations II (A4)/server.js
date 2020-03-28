d92cc9fe0400501149f42f2509b252f615d5dce8
Neil <neilkelkar1@gmail.com>
2020-03-28 15:34:59 -0400

    Initial commit

---
+++ Restaurant Reservations II (A4)/server.js
@@ -0,0 +1,108 @@
+/* E4 server.js */
+'use strict';
+const log = console.log;
+
+// Express
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+// Mongo and Mongoose
+const { ObjectID } = require('mongodb')
+const { mongoose } = require('./db/mongoose');
+const { Restaurant } = require('./models/restaurant')
+
+
+/// Route for adding restaurant, with *no* reservations (an empty array).
+/* 
+Request body expects:
+{
+	"name": <restaurant name>
+	"description": <restaurant description>
+}
+Returned JSON should be the database document added.
+*/
+// POST /restaurants
+app.post('/restaurants', (req, res) => {
+	// Add code here
+
+})
+
+
+/// Route for getting all restaurant information.
+// GET /restaurants
+app.get('/restaurants', (req, res) => {
+	// Add code here
+
+})
+
+
+/// Route for getting information for one restaurant.
+// GET /restaurants/id
+app.get('/restaurants/:id', (req, res) => {
+	// Add code here
+
+})
+
+
+/// Route for adding reservation to a particular restaurant.
+/* 
+Request body expects:
+{
+	"time": <time>
+	"people": <number of people>
+}
+*/
+// Returned JSON should have the updated restaurant database 
+//   document that the reservation was added to, AND the reservation subdocument:
+//   { "reservation": <reservation subdocument>, "restaurant": <entire restaurant document>}
+// POST /restaurants/id
+app.post('/restaurants/:id', (req, res) => {
+	// Add code here
+
+})
+
+
+/// Route for getting information for one reservation of a restaurant (subdocument)
+// GET /restaurants/id
+app.get('/restaurants/:id/:resv_id', (req, res) => {
+	// Add code here
+
+})
+
+
+/// Route for deleting reservation
+// Returned JSON should have the updated restaurant database
+//   document from which the reservation was deleted, AND the reservation subdocument deleted:
+//   { "reservation": <reservation subdocument>, "restaurant": <entire restaurant document>}
+// DELETE restaurant/<restaurant_id>/<reservation_id>
+app.delete('/restaurants/:id/:resv_id', (req, res) => {
+	// Add code here
+
+})
+
+
+/// Route for changing reservation information
+/* 
+Request body expects:
+{
+	"time": <time>
+	"people": <number of people>
+}
+*/
+// Returned JSON should have the updated restaurant database
+//   document in which the reservation was changed, AND the reservation subdocument changed:
+//   { "reservation": <reservation subdocument>, "restaurant": <entire restaurant document>}
+// PATCH restaurant/<restaurant_id>/<reservation_id>
+app.patch('/restaurants/:id/:resv_id', (req, res) => {
+	// Add code here
+
+})
+
+
+////////// DO NOT CHANGE THE CODE OR PORT NUMBER BELOW
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+});
